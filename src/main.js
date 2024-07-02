049d3d4be6b9cc0622970e9ecbd3e87848e55351
aliso4ka <gulyayevaalisa@gmail.com>
2024-07-02 16:30:13 +0200

    ff

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     const openMenuBtn = document.querySelector(".open-menu-btn");
     const closeMenuBtn = document.querySelector(".close-menu-btn");
     const mobMenu = document.querySelector(".mob-menu");
 
-    openMenuBtn.addEventListener("click", function() {
+    openMenuBtn.addEventListener("click", function () {
         mobMenu.classList.add("is-open");
     });
 
-    closeMenuBtn.addEventListener("click", function() {
+    closeMenuBtn.addEventListener("click", function () {
         mobMenu.classList.remove("is-open");
     });
 });
