98b9ef3839fc3835c25894925ff9924da821d639
aliso4ka <gulyayevaalisa@gmail.com>
2024-07-02 16:55:50 +0200

    scroll.js added

---
+++ src/scroll.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const navLinks = document.querySelectorAll('.alink');
+ 
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+
+            const targetId = this.getAttribute('href').substring(1); // Получаем id целевого элемента без #
+            const targetElement = document.getElementById(targetId); // Находим целевой элемент по его id
+
+            if (targetElement) {
+                // Вычисляем позицию элемента на странице
+                const offsetTop = targetElement.offsetTop;
+
+                // Плавная прокрутка к элементу
+                window.scrollTo({
+                    top: offsetTop,
+                    behavior: 'smooth' // Плавная анимация прокрутки
+                });
+            }
+        });
+    });
+
+
+});
