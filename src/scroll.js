bb98d22534dcab9838bdc0d0b11835874f8d2f36
aliso4ka <gulyayevaalisa@gmail.com>
2024-07-02 17:31:54 +0200

    link

--- src/scroll.js
+++ src/scroll.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     const navLinks = document.querySelectorAll('.alink');
- 
+ const navsLinks = document.querySelectorAll('.link');
 
     navLinks.forEach(link => {
         link.addEventListener('click', function(e) {
@@ -22,5 +22,24 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 
+    navsLinks.forEach(link => {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+
+            const targetId = this.getAttribute('href').substring(1); // Получаем id целевого элемента без #
+            const targetElement = document.getElementById(targetId); // Находим целевой элемент по его id
+
+            if (targetElement) {
+                // Вычисляем позицию элемента на странице
+                const offsetTop = targetElement.offsetTop;
+
+                // Плавная прокрутка к элементу
+                window.scrollTo({
+                    top: offsetTop,
+                    behavior: 'smooth' // Плавная анимация прокрутки
+                });
+            }
+        });
+    });
 
 });
