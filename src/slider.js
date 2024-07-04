3e89cac59481888bc3c4c84ed5ea97bae4f0d1a0
aliso4ka <gulyayevaalisa@gmail.com>
2024-07-04 18:46:31 +0200

    js

--- src/slider.js
+++ src/slider.js
@@ -1,10 +1,11 @@
-export function showImage(src, srcset2x) {
-  console.log('showImage called with src:', src, 'and srcset2x:', srcset2x);
-  let mainImage = document.getElementById('mainImage');
-  mainImage.src = src;
-  mainImage.srcset = `${src} 1x, ${srcset2x} 2x`;
-  mainImage.alt = src.split('/').pop().split('.')[0]; // Оновлення alt атрибуту
-}
+function showImage(src, srcset2x) {
+            console.log('showImage called with src:', src, 'and srcset2x:', srcset2x);
+            let mainImage = document.getElementById('mainImage');
+            mainImage.src = src;
+            mainImage.srcset = `${src} 1x, ${srcset2x} 2x`;
+            mainImage.alt = src.split('/').pop().split('.')[0]; // Update alt attribute
+            mainImage.style.display = 'block'; // Make the image visible if initially hidden
+        }
 
-// Для забезпечення доступу до функції через глобальний простір імен
-window.showImage = showImage;
+        // Ensure the function is accessible in the global namespace
+        window.showImage = showImage;
