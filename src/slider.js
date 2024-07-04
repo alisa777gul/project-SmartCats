09088cac1695c45505718f5f36f1177813bc3c37
aliso4ka <gulyayevaalisa@gmail.com>
2024-07-04 19:02:08 +0200

    try

--- src/slider.js
+++ src/slider.js
@@ -1,11 +1,13 @@
-function showImage(src, srcset2x) {
-            console.log('showImage called with src:', src, 'and srcset2x:', srcset2x);
-            let mainImage = document.getElementById('mainImage');
-            mainImage.src = src;
-            mainImage.srcset = `${src} 1x, ${srcset2x} 2x`;
-            mainImage.alt = src.split('/').pop().split('.')[0]; // Update alt attribute
-            mainImage.style.display = 'block'; // Make the image visible if initially hidden
-        }
 
-        // Ensure the function is accessible in the global namespace
-        window.showImage = showImage;
+        
+ function showImage(event) {
+        let mainImage = document.getElementById('mainImage');
+        let clickedImage = event.target;
+
+        mainImage.src = clickedImage.src;
+        mainImage.srcset = clickedImage.srcset;
+        mainImage.alt = clickedImage.alt;
+    }
+
+    // Ensure the function is accessible in the global namespace
+    window.showImage = showImage;
