db2806f1b07ec0536c81fff283783d19aeedf8d3
aliso4ka <gulyayevaalisa@gmail.com>
2024-07-04 19:56:23 +0200

    ready

--- src/slider.js
+++ src/slider.js
@@ -1,10 +1,13 @@
-
-    function showImage(clickedImage) {
+ function showImage(clickedImage) {
         let mainImage = document.getElementById('mainImage');
         
         mainImage.src = clickedImage.src;
         mainImage.srcset = clickedImage.srcset;
         mainImage.alt = clickedImage.alt;
+   mainImage.style.display = 'block';
+   
+        // Добавляем класс для увеличения картинки
+        mainImage.classList.add('zoom-in');
     }
 
     // Ensure the function is accessible in the global namespace
